296da81134562953688e79ee464c431d9ae44a75
Taciano Morais Silva <tacianosilva@gmail.com>
2021-02-26 02:06:27 -0300

    Aula 02 NLW 04 Node

--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 mkdir api
 cd api
 yarn init -y
+yarn dev
 ```
 
-Dependências
+## Dependências
 
 ```
 yarn add express
@@ -15,4 +16,20 @@ yarn add @types/express -D
 yarn add typescript -D
 yarn tsc --init
 yarn add ts-node-dev -D
+
+yarn add uuid
+yarn add @types/uuid -D
+```
+
+## TypeORM
+
+```
+yarn add typeorm reflect-metadata
+yarn add sqlite3
+```
+
+```
+yarn typeorm migrations:create -n CreateUsers
+yarn typeorm migration:run
+yarn typeorm migration:revert
 ```
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,13 @@
-import express, { request, response } from 'express';
+import 'reflect-metadata';
+import express from 'express';
+import { router } from './routes';
+
+import "./database";
+
 
 const app = express();
 
-app.get("/users", (request, response) => {
-    return response.send("Hello World - NLW04");
-});
-
-app.get("/", (request, response) => {
-    return response.json({message: "Hello World - NLW04"});
-});
-
-app.post("/", (request, response) => {
-    return response.json({message: "Os dados foram salvos com sucesso!"});
-});
+app.use(express.json());
+app.use(router)
 
 app.listen(3333, () => console.log("Server is running!"));
