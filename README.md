a5cd00bfba62cfba3976837bf4db6d2632b878dc
Taciano Morais Silva <tacianosilva@gmail.com>
2021-02-26 00:16:58 -0300

    Aula 01 NLW 04 Node

---
+++ README.md
@@ -0,0 +1,18 @@
+# NLW 4 Node.js
+
+```
+mkdir api
+cd api
+yarn init -y
+```
+
+Dependências
+
+```
+yarn add express
+yarn add @types/express -D
+
+yarn add typescript -D
+yarn tsc --init
+yarn add ts-node-dev -D
+```
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { request, response } from 'express';
+
+const app = express();
+
+app.get("/users", (request, response) => {
+    return response.send("Hello World - NLW04");
+});
+
+app.get("/", (request, response) => {
+    return response.json({message: "Hello World - NLW04"});
+});
+
+app.post("/", (request, response) => {
+    return response.json({message: "Os dados foram salvos com sucesso!"});
+});
+
+app.listen(3333, () => console.log("Server is running!"));
